51fe44fb6ee531768839a1a186e102b9ddab4540
RichardSForsterRGU <92517674+RichardSForsterRGU@users.noreply.github.com>
2021-10-24 19:52:42 +0100

    Update hello_world.py
    Added the name request and printed out hello name.

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,5 @@
-# Assessment 1 - print hello world
-print("hello world")
+# Assessment 1 part 2 - amend print hello world to accept a name and print hello name.
+#Accept name
+name=input("Enter a name ")
+# Print hell and the variable 'name'.
+print(f"hello {name}")
